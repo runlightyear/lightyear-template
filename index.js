4e4b80d0d6e9cb518471387d26e874d7f342bf78
Eric Bouck <eric.bouck@gmail.com>
2023-05-02 16:19:22 -0700

    Add getDeployList to exports

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import { handler } from "@runlightyear/lightyear";
+import { handler, getDeployList } from "@runlightyear/lightyear";
 import "./src";
 
 exports.handler = handler;
 global.handler = handler;
+
+exports.getDeployList = getDeployList;
+global.getDeployList = getDeployList;
