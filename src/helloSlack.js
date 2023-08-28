4d8f83addbafd3128641b4457b3eab70e3b17726
Eric Bouck <eric.bouck@gmail.com>
2023-08-28 14:52:54 -0700

    Get rid of description for now

--- src/helloSlack.js
+++ src/helloSlack.js
@@ -4,7 +4,6 @@ import { Slack } from "@runlightyear/slack";
 defineAction({
   name: "helloSlack",
   title: "Hello Slack",
-  description: "Send a message to Slack",
   apps: ["slack"],
   run: async ({ auths }) => {
     const slack = new Slack({ auth: auths.slack });
